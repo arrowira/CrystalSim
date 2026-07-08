e0eb4d9e840a530c451fca5a1d035dfa94b436f4
arrowira <41136800+arrowira@users.noreply.github.com>
2026-07-08 01:17:28 -0700

    incorrect rotation logic but the systems kinda work

--- crystal-sim/molecule.gd
+++ crystal-sim/molecule.gd
@@ -2,23 +2,37 @@ extends Node2D
 
 var nearbyMolecules = []
 var underInfluence = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+func distance(vecA, vecB):
+	return sqrt(pow(vecB.x-vecA.x,2)+pow(vecB.y-vecA.y,2))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if len(nearbyMolecules)!=0:
 		underInfluence = true
+		
+		var rotAdd = 0
+		for molecule in nearbyMolecules:
+			var negativeDistance = distance(molecule.getNegative(),getNegative())
+			var positiveDistance = distance(molecule.getPositive(),getPositive())
+			
+			if rotation-molecule.rotation < 0:
+				rotAdd += abs(negativeDistance-positiveDistance)
+			else:
+				rotAdd -= abs(negativeDistance-positiveDistance)
+		rotate(Engine.time_scale*rotAdd/1000.0)
 	else:
 		underInfluence = false
 	
-
+func getNegative():
+	return $negative.global_position
+func getPositive():
+	return $positive.global_position
 
 func _on_mol_area_area_entered(area: Area2D) -> void:
 	if area.name == "molArea":
-		nearbyMolecules.append(area)
+		nearbyMolecules.append(area.get_parent())
 
 
 
